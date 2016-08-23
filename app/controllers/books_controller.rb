1b4c723a44f924d2e079929d82e489d71f5aff2c
Mat <geilermathieu@gmail.com>
2016-08-23 15:45:34 +0200

    bookcontroller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+
+  before_action :find_book, only: [:edit, :destroy, :update, :show]
+
+  def index
+    @books = Book.all
+    @checkups = Checkup.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    redirect_to books_path
+  end
+
+  def edit
+  end
+
+  def update
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:registration_plate, :initial_km, :initial_entry_into_service, :circulation_date)
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
+
+end
