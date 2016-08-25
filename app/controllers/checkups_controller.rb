3446d7f897bbfdaf592a373791d07a3bfd7f3153
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-25 21:56:12 +0200

    ajout check si la personne qui ajoute un checkup est garagiste

--- app/controllers/checkups_controller.rb
+++ app/controllers/checkups_controller.rb
@@ -20,6 +20,9 @@ class CheckupsController < ApplicationController
     @checkup.km_ondate = checkup_params[:km_ondate]
     @checkup.checkup_item_id = checkup_params[:checkup_item].to_i
     @checkup.book = Book.find(params[:book_id])
+     if current_user.garagiste
+        @checkup.garage = current_user.garages.first
+    end
     authorize @checkup
     @checkup.save
     redirect_to book_checkups_path
