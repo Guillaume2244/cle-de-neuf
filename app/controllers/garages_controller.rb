6de883066cfe56a3750ace59930bbee2af19828f
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-26 00:52:26 +0200

    done qq modifs dans pundit a faire

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -16,15 +16,35 @@ class GaragesController < ApplicationController
    end
   end
 
+
   def index
     @garages = policy_scope(Garage)
     @garage = @garages.first
     @checkups = @garage.checkups
+    @books = Book.all
+    @books.each do |book|
+      if book.registration_plate == @garage.registration_plate && book.token == @garage.token
+        @find = book
+      end
+    end
+  end
+
+  def edit
+    @garage = Garage.find(params[:id])
+    authorize @garage
+  end
+
+  def update
+    @garage = Garage.find(params[:id])
+    @garage.update(garage_params)
+    authorize @garage
+    redirect_to garages_path
   end
 
   private
 
   def garage_params
-    params.require(:garage).permit(:name, :siret, :address, :city, :zip_code, :phone_number)
+    params.require(:garage).permit(:name, :siret, :address, :city, :zip_code, :phone_number, :token, :registration_plate)
   end
+
 end
