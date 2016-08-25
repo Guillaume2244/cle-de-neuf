3a1dc4115f4acccf184b838bd3f758f0e1d8a106
Mat <geilermathieu@gmail.com>
2016-08-25 16:21:23 +0200

    setuptheme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,14 @@
 //= require jquery
 //= require jquery_ujs
+//= require app
 //= require bootstrap-sprockets
-//= require_tree .
+//= require plugins/btn-effects
+//= require plugins/circles-master
+//= require plugins/datepicker
+//= require plugins/form-sliders
+//= require plugins/masking
+//= require plugins/vector-map
+//= require plugins/validation
+
+
+
