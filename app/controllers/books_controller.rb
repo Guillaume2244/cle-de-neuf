75981c843b7c0f09166943a02c8878ffa159e9f1
Mat <geilermathieu@gmail.com>
2016-09-01 15:39:24 +0200

    to commmmmit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -73,220 +73,220 @@ class BooksController < ApplicationController
   end
 
   def pneus
-     a = @book.template.pneus_date.to_i
+   a = @book.template.pneus_date.to_i
 
-    if  (@book.circulation_date + a * 365 - Date.today).to_i < 0
-      n = (@book.initial_km.to_i / @book.template.pneus_km.to_i).round
-      z = - ((@book.circulation_date + a * 365 - Date.today).to_i + (n * 365))
-        if z < 0
-        z = ((@book.circulation_date + a * 365 - Date.today).to_i + (n * 365))
-        end
-        v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = v
-      @c.checkup_item = CheckupItem.where(name:"Pneus", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = v + z
-      @c.checkup_item = CheckupItem.where(name:"Pneus", numero: n + 2).first
-      @c.save
-    else
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + (a * 365)
-      @c.checkup_item = CheckupItem.where(name:"Pneus", numero: 1).first
-      @c.save
+   if  (@book.circulation_date + a * 365 - Date.today).to_i < 0
+    n = (@book.initial_km.to_i / @book.template.pneus_km.to_i).round
+    z = - ((@book.circulation_date + a * 365 - Date.today).to_i + (n * 365))
+    if z < 0
+      z = ((@book.circulation_date + a * 365 - Date.today).to_i + (n * 365))
     end
+    v = Date.today + z
+    new_checkup_not_done
+    @c.estimated_date_string = v
+    @c.checkup_item = CheckupItem.where(name:"Pneus", numero: n + 1).first
+    @c.save
+    new_checkup_not_done
+    @c.estimated_date_string = v + z
+    @c.checkup_item = CheckupItem.where(name:"Pneus", numero: n + 2).first
+    @c.save
+  else
+    new_checkup_not_done
+    @c.estimated_date_string = @book.circulation_date + (a * 365)
+    @c.checkup_item = CheckupItem.where(name:"Pneus", numero: 1).first
+    @c.save
   end
+end
 
-  def freinage
-     b = @book.template.freinage.to_i
+def freinage
+ b = @book.template.freinage.to_i
 
-     if (@book.circulation_date + b * 365 - Date.today).to_i < 0
-      n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.freinage.to_i
-      z = - ((@book.circulation_date + b * 365 - Date.today).to_i + (n * 365))
-       if z < 0
-        z = ((@book.circulation_date + b * 365 - Date.today).to_i + (n * 365))
-       end
-       v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((b * n + 1) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Freinage", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((b * n + 2) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Freinage", numero: n + 2).first
-      @c.save
-    else
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + (2 * 365)
-      @c.checkup_item = CheckupItem.where(name:"Freinage", numero: 1).first
-      @c.save
+ if (@book.circulation_date + b * 365 - Date.today).to_i < 0
+  n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.freinage.to_i
+  z = - ((@book.circulation_date + b * 365 - Date.today).to_i + (n * 365))
+  if z < 0
+    z = ((@book.circulation_date + b * 365 - Date.today).to_i + (n * 365))
+  end
+  v = Date.today + z
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((b * n + 1) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Freinage", numero: n + 1).first
+  @c.save
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((b * n + 2) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Freinage", numero: n + 2).first
+  @c.save
+else
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + (2 * 365)
+  @c.checkup_item = CheckupItem.where(name:"Freinage", numero: 1).first
+  @c.save
+end
+
+end
+
+
+def revision
+  c = @book.template.revision_date.to_i
+  if (@book.circulation_date + c * 365 - Date.today).to_i < 0
+    n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.revision_date.to_i
+    z = - ((@book.circulation_date + c * 365 - Date.today).to_i + (n * 365))
+    if z < 0
+      z = ((@book.circulation_date + c * 365 - Date.today).to_i + (n * 365))
     end
-
+    v = Date.today + z
+    new_checkup_not_done
+    @c.estimated_date_string = @book.circulation_date + ((c * n + 1) * 365)
+    @c.checkup_item = CheckupItem.where(name:"Revision", numero: n + 1).first
+    @c.save
+    new_checkup_not_done
+    @c.estimated_date_string = @book.circulation_date + ((c * n + 2) * 365)
+    @c.checkup_item = CheckupItem.where(name:"Revision", numero: n + 2).first
+    @c.save
   end
+end
 
 
-  def revision
-    c = @book.template.revision_date.to_i
-    if (@book.circulation_date + c * 365 - Date.today).to_i < 0
-      n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.revision_date.to_i
-       z = - ((@book.circulation_date + c * 365 - Date.today).to_i + (n * 365))
-        if z < 0
-        z = ((@book.circulation_date + c * 365 - Date.today).to_i + (n * 365))
-        end
-        v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((c * n + 1) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Revision", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((c * n + 2) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Revision", numero: n + 2).first
-      @c.save
+def balai
+ d = @book.template.balai.to_i
+ if (@book.circulation_date + d * 365 - Date.today).to_i < 0
+  n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.balai.to_i
+  z = - ((@book.circulation_date + d * 365 - Date.today).to_i + (n * 365))
+  if z < 0
+    z = ((@book.circulation_date + d * 365 - Date.today).to_i + (n * 365))
+  end
+  v = Date.today + z
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((d * n + 1) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Balai essui glace", numero: n + 1).first
+  @c.save
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((d * n + 2) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Balai essui glace", numero: n + 2).first
+  @c.save
+end
+end
+
+def echappement
+ e = @book.template.echappement.to_i
+ if (@book.circulation_date + e * 365 - Date.today).to_i < 0
+  n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.echappement.to_i
+  z = - ((@book.circulation_date + e * 365 - Date.today).to_i + (n * 365))
+  if z < 0
+    z = ((@book.circulation_date + e * 365 - Date.today).to_i + (n * 365))
+  end
+  v = Date.today + z
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((e * n + 1) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Echappement", numero: n + 1).first
+  @c.save
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((e * n + 2) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Echappement", numero: n + 2).first
+  @c.save
+else
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + (2 * 365)
+  @c.checkup_item = CheckupItem.where(name:"Echappement", numero: 1).first
+  @c.save
+end
+end
+
+def amortisseurs
+  f = @book.template.amortisseurs_date.to_i
+  if (@book.circulation_date + f * 365 - Date.today).to_i < 0
+    n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.amortisseurs_date.to_i
+    z = - ((@book.circulation_date + f * 365 - Date.today).to_i + (n * 365))
+    if z < 0
+      z = ((@book.circulation_date + f * 365 - Date.today).to_i + (n * 365))
     end
+    v = Date.today + z
+    new_checkup_not_done
+    @c.estimated_date_string = @book.circulation_date + ((f * n + 1) * 365)
+    @c.checkup_item = CheckupItem.where(name:"Amortisseurs", numero: n + 1).first
+    @c.save
+    new_checkup_not_done
+    @c.estimated_date_string = @book.circulation_date + ((f * n + 2) * 365)
+    @c.checkup_item = CheckupItem.where(name:"Amortisseurs", numero: n + 2).first
+    @c.save
   end
+end
 
-
-  def balai
-     d = @book.template.balai.to_i
-    if (@book.circulation_date + d * 365 - Date.today).to_i < 0
-      n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.balai.to_i
-        z = - ((@book.circulation_date + d * 365 - Date.today).to_i + (n * 365))
-         if z < 0
-          z = ((@book.circulation_date + d * 365 - Date.today).to_i + (n * 365))
-         end
-       v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((d * n + 1) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Balai essui glace", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((d * n + 2) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Balai essui glace", numero: n + 2).first
-      @c.save
-    end
+def bougies
+  if @book.initial_km.to_i < @book.template.bougies_km.to_i
+    n = (@book.initial_km.to_i / @book.template.amortisseurs_km.to_i).round
+    v = (Date.today + 2 * 365)
+    new_checkup_not_done
+    @c.estimated_date_string = v
+    @c.checkup_item = CheckupItem.where(name:"Bougies", numero: 1).first
+    @c.save
   end
+end
 
-  def echappement
-     e = @book.template.echappement.to_i
-    if (@book.circulation_date + e * 365 - Date.today).to_i < 0
-      n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.echappement.to_i
-        z = - ((@book.circulation_date + e * 365 - Date.today).to_i + (n * 365))
-        if z < 0
-        z = ((@book.circulation_date + e * 365 - Date.today).to_i + (n * 365))
-        end
-      v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((e * n + 1) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Echappement", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((e * n + 2) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Echappement", numero: n + 2).first
-      @c.save
-    else
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + (2 * 365)
-      @c.checkup_item = CheckupItem.where(name:"Echappement", numero: 1).first
-      @c.save
-    end
+def climatisation
+ g = @book.template.climatisation.to_i
+ if (@book.circulation_date + g * 365 - Date.today).to_i < 0
+  n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.climatisation.to_i
+  z = - ((@book.circulation_date + g * 365 - Date.today).to_i + (n * 365))
+  if z < 0
+    z = ((@book.circulation_date + g * 365 - Date.today).to_i + (n * 365))
   end
+  v = Date.today + z
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((g * n + 1) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Climatisation", numero: n + 1).first
+  @c.save
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((g * n + 2) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Climatisation", numero: n + 2).first
+  @c.save
+end
+end
 
-  def amortisseurs
-    f = @book.template.amortisseurs_date.to_i
-   if (@book.circulation_date + f * 365 - Date.today).to_i < 0
-      n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.amortisseurs_date.to_i
-       z = - ((@book.circulation_date + f * 365 - Date.today).to_i + (n * 365))
-       if z < 0
-        z = ((@book.circulation_date + f * 365 - Date.today).to_i + (n * 365))
-      end
-      v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((f * n + 1) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Amortisseurs", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((f * n + 2) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Amortisseurs", numero: n + 2).first
-      @c.save
-    end
+def controle_technique_f
+ h = @book.template.controle_technique_others.to_i
+ if (@book.circulation_date + (h + 2) * 365 - Date.today).to_i < 0
+  n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.controle_technique_first.to_i
+  z = - ((@book.circulation_date + h * 365 - Date.today).to_i + (n * 365))
+  if z < 0
+    z = ((@book.circulation_date + h * 365 - Date.today).to_i + (n * 365))
   end
+  v = Date.today + z
+  new_checkup_not_done
+  @c.estimated_date_string = v
+  @c.checkup_item = CheckupItem.where(name:"Controle Technique", numero: n + 1).first
+  @c.save
+  new_checkup_not_done
+  @c.estimated_date_string = v
+  @c.checkup_item = CheckupItem.where(name:"Controle Technique", numero: n + 2).first
+  @c.save
+else
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + (h + 2) * 365
+  @c.checkup_item = CheckupItem.where(name:"Controle Technique", numero: 1).first
+  @c.save
+end
 
-  def bougies
-    if @book.initial_km.to_i < @book.template.bougies_km.to_i
-      n = (@book.initial_km.to_i / @book.template.amortisseurs_km.to_i).round
-      v = (Date.today + 2 * 365)
-      new_checkup_not_done
-      @c.estimated_date_string = v
-      @c.checkup_item = CheckupItem.where(name:"Bougies", numero: 1).first
-      @c.save
-    end
+end
+
+def vidange
+  j = @book.template.vidange_date.to_i
+  if  (@book.circulation_date + j * 365 - Date.today).to_i < 0
+   n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.vidange_date.to_i
+   z = - ((@book.circulation_date + j * 365 - Date.today).to_i + (n * 365))
+   if z < 0
+    z = ((@book.circulation_date + j * 365 - Date.today).to_i + (n * 365))
   end
-
-  def climatisation
-     g = @book.template.climatisation.to_i
-    if (@book.circulation_date + g * 365 - Date.today).to_i < 0
-      n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.climatisation.to_i
-       z = - ((@book.circulation_date + g * 365 - Date.today).to_i + (n * 365))
-       if z < 0
-        z = ((@book.circulation_date + g * 365 - Date.today).to_i + (n * 365))
-       end
-      v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((g * n + 1) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Climatisation", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((g * n + 2) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Climatisation", numero: n + 2).first
-      @c.save
-    end
-  end
-
-  def controle_technique_f
-     h = @book.template.controle_technique_others.to_i
-    if (@book.circulation_date + (h + 2) * 365 - Date.today).to_i < 0
-      n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.controle_technique_first.to_i
-        z = - ((@book.circulation_date + h * 365 - Date.today).to_i + (n * 365))
-       if z < 0
-        z = ((@book.circulation_date + h * 365 - Date.today).to_i + (n * 365))
-       end
-       v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = v
-      @c.checkup_item = CheckupItem.where(name:"Controle Technique", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = v
-      @c.checkup_item = CheckupItem.where(name:"Controle Technique", numero: n + 2).first
-      @c.save
-    else
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + (h + 2) * 365
-      @c.checkup_item = CheckupItem.where(name:"Controle Technique", numero: 1).first
-      @c.save
-   end
-
-  end
-
-  def vidange
-    j = @book.template.vidange_date.to_i
-    if  (@book.circulation_date + j * 365 - Date.today).to_i < 0
-       n = (Date.today - @book.circulation_date).to_i / 365 / @book.template.vidange_date.to_i
-        z = - ((@book.circulation_date + j * 365 - Date.today).to_i + (n * 365))
-      if z < 0
-        z = ((@book.circulation_date + j * 365 - Date.today).to_i + (n * 365))
-       end
-       v = Date.today + z
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((j * n + 1) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Vidange", numero: n + 1).first
-      @c.save
-      new_checkup_not_done
-      @c.estimated_date_string = @book.circulation_date + ((j * n + 2) * 365)
-      @c.checkup_item = CheckupItem.where(name:"Vidange", numero: n + 2).first
-      @c.save
-    end
-  end
+  v = Date.today + z
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((j * n + 1) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Vidange", numero: n + 1).first
+  @c.save
+  new_checkup_not_done
+  @c.estimated_date_string = @book.circulation_date + ((j * n + 2) * 365)
+  @c.checkup_item = CheckupItem.where(name:"Vidange", numero: n + 2).first
+  @c.save
+end
+end
 
 end
